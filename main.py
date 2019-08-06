1321efe8bc8025566a47f6913d23f811829c1912
bleveque <bleveque@student.42.fr>
2019-08-06 18:30:25 +0200

    random field generation ok

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pygame 
+import numpy as np 
+import sys
+import re
+import field
+import viewer
+
+np.set_printoptions(threshold=sys.maxsize)
+def parser():
+    lines = sys.stdin.read()
+    parentheses = re.findall('\((.*?)\)', lines)
+    needed_points = np.array([x.split(",") for x in parentheses]).astype(np.float)
+    #print(needed_points)
+    #print(needed_points.shape)
+    return (needed_points)
+
+if __name__ == "__main__" :
+    needed_points = parser()
+    map = field.Field(needed_points, 75)
+    map.resize()
+    map.place_needed_points()
+    map.gen_all_points()
+    map.transform_all()
+   # print(map.map_proj)
+    viewer = viewer.Viewer(1000, 1000, map)
+    viewer.initialization()
+    viewer.run()
+
+#l.22 map = field.Field(needed_points, 50) <=>  def __init__(self, needed_points, size=100, power=2) a size ?
+ 
